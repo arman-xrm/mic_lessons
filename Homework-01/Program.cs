fb3a378ffbbf84550acf19c7d775791b75154cef
Arman Hovhannisyan <armanhovh26@gmail.com>
2021-02-12 18:22:59 +0400

    homework

--- Homework-01/Program.cs
+++ Homework-01/Program.cs
@@ -12,132 +12,165 @@ namespace Homework_01
             Console.WriteLine("===From Automat press 0 === From manual press 1===");
             int g = int.Parse(Console.ReadLine());
             int[] arr = arr = GetArrayAutomat(n); ;
-            if (g == 0){
+            if (g == 0)
+            {
                 arr = GetArrayAutomat(n);
 
-            }else if (g == 1){
+            }
+            else if (g == 1)
+            {
                 arr = GetArrayManual(n);
-            }else{
+            }
+            else
+            {
                 Console.WriteLine("=====================Read attentively====================");
             }
 
-Console.WriteLine("=================My Array======================");
-PrintArray(arr);
+            Console.WriteLine("=================My Array======================");
+            PrintArray(arr);
 
-Console.WriteLine("==================Sum of Array========================");
-Console.WriteLine(GetSumArrayElements(arr));
+            Console.WriteLine("==================Sum of Array========================");
+            Console.WriteLine(GetSumArrayElements(arr));
 
-Console.WriteLine("==================Max Element of Array========================");
-Console.WriteLine(GetMaxElement(arr));
+            Console.WriteLine("==================Max Element of Array========================");
+            Console.WriteLine(GetMaxElement(arr));
 
-Console.WriteLine("==================Max Element of Array====================");
-Console.WriteLine(GetMinElement(arr));
+            Console.WriteLine("==================Max Element of Array====================");
+            Console.WriteLine(GetMinElement(arr));
 
-//int[] arrPrime = GetPrimeArray(arr);
-//Console.WriteLine("==================Prime Array========================");
-//PrintArray(arrPrime);
+            int[] arrPrime = GetPrimeArray(n);
+            Console.WriteLine("==================Prime Array========================");
+            PrintArray(arrPrime);
 
-PrintArray(SortArray(arr));
+            PrintArray(SortArray(arr));
 
         }
         private static int[] SortArray(int[] arr)
-{
-    int temp;
-    for (int i = 0; i < arr.Length - 1; i++)
-    {
-        for (int j = i + 1; j < arr.Length; j++)
         {
-            if (arr[i] > arr[j])
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
             {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        private static int GetMinElement(int[] arr)
+        {
+            int min = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            return min;
+        }
+
+        private static int[] GetPrimeArray(int n)
+        {
+            int[] arr = new int[n];
+            //Random rnd = new Random();
+
+            int needStop = 0;
+            do
+            {
+                int v = new Random().Next(1, 100);//.rnd.Next(-20, 50); 
+                bool isPrime = Prime(v);
+                if (isPrime)
+                {
+                    arr[needStop] = v;
+                    needStop++;
+                }
+
+            } while (needStop == n);
+            return arr;
+        }
+
+        static bool Prime(int input)
+        {
+            for (int i = 2; i <= input / 2; i++)
+            {
+                if (input % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        private static int GetMaxElement(int[] arr)
+        {
+            int max = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            return max;
+        }
+
+        private static int GetSumArrayElements(int[] arr)
+        {
+            int sum = default;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+                //Console.WriteLine("arr[{0}] = {1}", i, arr[i]);
             }
         }
-    }
-    return arr;
-}
 
-private static int GetMinElement(int[] arr)
-{
-    int min = arr[0];
-
-    for (int i = 1; i < arr.Length; i++)
-    {
-        if (arr[i] < min)
+        private static int[] GetArrayAutomat(int n)
         {
-            min = arr[i];
+            int[] arr = new int[n];
+            //Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = new Random().Next(-20, 50);//.rnd.Next(-20, 50);            
+            }
+
+            return arr;
         }
-    }
 
-    return min;
-}
-
-private static int[] GetPrimeArray(int[] arr)
-{
-    throw new NotImplementedException();
-}
-
-private static int GetMaxElement(int[] arr)
-{
-    int max = arr[0];
-
-    for (int i = 1; i < arr.Length; i++)
-    {
-        if (arr[i] > max)
+        private static int[] GetArrayManual(int n)
         {
-            max = arr[i];
+            int[] arr = new int[n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"arr[{i}] = ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            return arr;
         }
-    }
-
-    return max;
-}
-
-private static int GetSumArrayElements(int[] arr)
-{
-    int sum = default;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        sum += arr[i];
-    }
-
-    return sum;
-}
-
-private static void PrintArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.WriteLine($"arr[{i}] = {arr[i]}");
-        //Console.WriteLine("arr[{0}] = {1}", i, arr[i]);
-    }
-}
-
-private static int[] GetArrayAutomat(int n)
-{
-    int[] arr = new int[n];
-    //Random rnd = new Random();
-
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = new Random().Next(-20, 50);//.rnd.Next(-20, 50);            
-    }
-
-    return arr;
-}
-
-private static int[] GetArrayManual(int n)
-{
-    int[] arr = new int[n];
-
-
-    for (int i = 0; i < n; i++)
-    {
-        Console.Write($"arr[{i}] = ");
-        arr[i] = int.Parse(Console.ReadLine());
-    }
-
-    return arr;
-}
     }
 }
