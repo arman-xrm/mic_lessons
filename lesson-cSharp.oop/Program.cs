bb80cc454c781fb36d93c37f4f31edd87685320c
Arman Hovhannisyan <armanhovh26@gmail.com>
2021-02-12 17:21:05 +0400

    change

--- lesson-cSharp.oop/Program.cs
+++ lesson-cSharp.oop/Program.cs
@@ -6,7 +6,7 @@ namespace lesson_cSharp.oop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World jkkjkjjkjkjjkjkjk!");
         }
     }
 }
